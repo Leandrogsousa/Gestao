124c193bfc9cdf5e11ab48c26c0379d6dfec53d3
Leandro Sousa <leandrogomyys@gmail.com>
2023-02-07 22:01:23 -0300

    Projeto Gestão
    Criando novo projeto de um sistema

---
+++ Configuracao/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace Models
+{
+    public class Usuario
+    {
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Passoword { get; set; }
+        public string? Email  { get; set; }
+        public string? Cpf { get; set; }
+        public bool Ativo { get; set; }
+
+        public List <GrupoUsuario> GrupoUsuarios { get; set; }
+
+
+
+    }
+}
