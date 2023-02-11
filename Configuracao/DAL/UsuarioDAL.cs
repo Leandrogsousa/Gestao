ae75bf674c6aad1edef9fc1e184848fd8abcc36f
Leandro Sousa <leandrogomyys@gmail.com>
2023-02-10 21:54:02 -0300

    Projeto Configuração
    Atualização projeto , inserindo conexão

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -13,10 +13,25 @@ namespace DAL
             {
                 cn.ConnectionString = Conexao.StringDeConexao;
                 SqlCommand cmd = new SqlCommand();    
-            }
-            catch (Exception)
-            {
+                cmd.Connection = cn;
+                cmd.CommandText = @"Insert Into Usuario(Nome,Username,CPF,Email,Passoword,Ativo)
+                                  Values (@Nome,@Username,@CPF,@Email,@Passoword,@Ativo)";
 
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", _usuario.Nome);
+                cmd.Parameters.AddWithValue("@Username", _usuario.Username);
+                cmd.Parameters.AddWithValue("@CPF", _usuario.Cpf);
+                cmd.Parameters.AddWithValue("@Email", _usuario.Email);
+                cmd.Parameters.AddWithValue("@Password", _usuario.Passoword);
+                cmd.Parameters.AddWithValue("@Ativo", _usuario.Ativo);
+
+                cn.Open();
+                cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um Usuário no banco: " + ex.Message);
             }
             finally
             {
