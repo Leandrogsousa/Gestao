6353cf1ffe95a1f5a8c0508370f8079a012d2f9b
Leandro Sousa <leandrogomyys@gmail.com>
2023-02-13 22:01:27 -0300

    Atualização projeto
    Continuação do projeto

---
+++ Configuracao/BLL/UsuarioBLL.cs
@@ -0,0 +1,50 @@
+﻿using DAL;
+using Models;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+            if (_usuario.Username.Length <=3 || _usuario.Username.Length >= 50)
+            {
+                throw new Exception(" O nome de usuário deve ter mais de três caracteres.");
+            }
+
+            if (_usuario.Username.Contains(" "))
+            {
+                throw new Exception("O nome não pode conter espaços.");
+            }
+
+            if (_usuario.Passoword.Contains("1234567"))
+            {
+                throw new Exception("Não é permitido um número sequencial.");
+            }
+
+            if (_usuario.Passoword.Length < 7 || _usuario.Passoword.Length > 11)
+            {
+                throw new Exception("A senha deve ter entre 7 e 11 caracteres.");
+            }
+
+
+            //TODO: Validar se a senha é 1234.
+
+
+            UsuarioDAL usuarioDAL = new UsuarioDAL();
+            usuarioDAL.Inserir(_usuario);
+        }
+        public  Usuario Buscar(string _nomeUsuario)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(Usuario _usuario)
+        {
+            
+        }
+        public void Excluir(Usuario _usuario)
+        {
+
+        }
+    }
+}
--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -1,17 +1,21 @@
-﻿namespace ConsoleAppPrincipal
+﻿using BLL;
+using Models;
+
+namespace ConsoleAppPrincipal
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int x;
-            int y;
-            int z;
-
-            while (true)
-            {
-
-            }
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+            Usuario usuario = new Usuario();
+            usuario.Nome = "Leandro Sousa";
+            usuario.Username = "leandro";
+            usuario.Ativo = true;
+            usuario.Email = "Leandro.g.sousa@senai.com";
+            usuario.Cpf = "954.151.16.14";
+            usuario.Passoword = "123";
+            usuarioBLL.Inserir(usuario);
         }
     }
 }
