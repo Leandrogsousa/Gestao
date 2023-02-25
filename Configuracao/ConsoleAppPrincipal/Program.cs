ada5c534f0e1f6047634ab244418df008ca7fd0e
Leandro Sousa <leandrogomyys@gmail.com>
2023-02-24 21:02:27 -0300

    Atualização projeto
    Continuação

--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -8,43 +8,90 @@ namespace ConsoleAppPrincipal
     {
         public static void Main(string[] args)
         {
+            int opcao = 0;
+            Console.WriteLine(@"Escolha uma opção:
+                            \n\n1 - Cadastrar usuário
+                            \n2 - Excluir usuário
+                            \n3 - Buscar todos os usuários");
 
-            try
-            {   
-                Usuario usuario = new Usuario();
-                UsuarioBLL usuarioBLL = new UsuarioBLL();
 
-                Console.WriteLine("Digite [1] para cadastrar usuário ou [0] para sair");
-                int X;
-                X = Convert.ToInt32(Console.ReadLine());
-                while (X != 0)
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    CadastrarUsuario();
+                    break;
+                case 2:
+                    ExcluirUsuario();
+                    break;
+                case 3:
+                    BuscarTodosOsUsuarios();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void ExcluirUsuario()
+        {
+            Console.WriteLine("Excluir usuário");
+        }
+
+        private static void BuscarTodosOsUsuarios()
+        {
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+            List<Usuario> usuarios = usuarioBLL.BuscarTodos();
+
+            foreach (Usuario item in usuarios)
+            {
+                Console.WriteLine("Id: " + item.id_usuario);
+                Console.WriteLine("Nome de usuário: " + item.Username);
+                Console.WriteLine("E-mail: " + item.Email);
+            }
+
+        }
+
+
+        private static void CadastrarUsuario()
+            {
+
+                try
                 {
+                    Usuario usuario = new Usuario();
+                    UsuarioBLL usuarioBLL = new UsuarioBLL();
 
-                    Console.WriteLine("Informe o seu Nome:");
-                    usuario.Nome = Console.ReadLine();
-                    Console.WriteLine("Informe o seu usuário:");
-                    usuario.Username = Console.ReadLine();
-                    Console.WriteLine("Informe a sua senha:");
-                    usuario.Passoword = Console.ReadLine();
-                    Console.WriteLine("Informe o seu Email:");
-                    usuario.Email = Console.ReadLine();
-                    Console.WriteLine("Informe o seu CPF:");
-                    usuario.Cpf = Console.ReadLine();
-                    Console.WriteLine("O usuário está ativo [S] ou [N]");
-                    usuario.Ativo = Console.ReadLine().ToUpper()=="S";
-
-                    Console.WriteLine("\n\nDigite [1] para cadastrar mais um  usuário ou [0] para sair");
+                    Console.WriteLine("Digite [1] para cadastrar usuário ou [0] para sair");
+                    int X;
                     X = Convert.ToInt32(Console.ReadLine());
+                    while (X != 0)
+                    {
+
+                        Console.WriteLine("Informe o seu Nome:");
+                        usuario.Nome = Console.ReadLine();
+                        Console.WriteLine("Informe o seu usuário:");
+                        usuario.Username = Console.ReadLine();
+                        Console.WriteLine("Informe a sua senha:");
+                        usuario.Passoword = Console.ReadLine();
+                        Console.WriteLine("Informe o seu Email:");
+                        usuario.Email = Console.ReadLine();
+                        Console.WriteLine("Informe o seu CPF:");
+                        usuario.Cpf = Console.ReadLine();
+                        Console.WriteLine("O usuário está ativo [S] ou [N]");
+                        usuario.Ativo = Console.ReadLine().ToUpper() == "S";
+
+                        Console.WriteLine("\n\nDigite [1] para cadastrar mais um  usuário ou [0] para sair");
+                        X = Convert.ToInt32(Console.ReadLine());
+                    }
+
+
+                    usuarioBLL.Inserir(usuario);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
 
-                
-                usuarioBLL.Inserir(usuario);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            
         }
     }
-}
